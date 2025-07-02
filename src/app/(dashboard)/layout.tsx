d56a8603bc573b2fccb8b441b6e2d0ced76e16b8
Arnab Ghosh <garnab559@gmail.com>
2025-07-02 12:51:18 +0000

    lovely, can u add this logo to the app in the required place (in header)

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from '@/lib/utils';
 import { useState, useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 import { Loader } from '@/components/loader';
+import { Icons } from '@/components/icons';
 
 export default function DashboardLayout({
   children,
@@ -26,20 +27,20 @@ export default function DashboardLayout({
           <Loader className="h-16 w-16" />
         </div>
       )}
-      <header className="sticky top-0 z-10 flex h-auto flex-col items-center gap-6 border-b border-white/10 bg-background px-10 py-20 sm:px-8 mb-30">
+      <header className="sticky top-0 z-10 mb-8 flex h-auto flex-col items-center gap-6 border-b border-white/10 bg-background px-10 py-8 sm:px-8">
         <div className="text-center">
-          <h1 className="text-4xl font-bold text-white">
-            ⚡InternSpark⚡
-          </h1>
+          <div className="flex items-center justify-center gap-x-3">
+            <Icons.InternSparkLogo className="h-14 w-14" />
+            <h1 className="text-4xl font-bold text-white">InternSpark</h1>
+          </div>
           <p
             className={cn(
               'mt-2 bg-gradient-to-r from-violet-400 to-indigo-500 bg-clip-text text-lg font-semibold text-transparent'
             )}
           >
-          Prep smart. Stand out. Land your dream internship
+            Prep smart. Stand out. Land your dream internship
           </p>
-          <p> Ace your internship preparation with Gemini powered IntenSpark
-          </p>
+          <p>Ace your internship preparation with Gemini powered IntenSpark</p>
         </div>
         <NavLinks setIsLoading={setIsLoading} />
       </header>
